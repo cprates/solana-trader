2956e8e2a7b7207669b6814676538b069e664340
cprates <cpr4t3s@gmail.com>
2022-03-27 16:21:57 +0100

    [client] Add option to setup all base accounts
    This should make testing easier.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,13 @@
 use borsh::BorshSerialize;
+use solana_program::program_pack::Pack;
 use solana_program::system_program;
 use trader_client::{Error, Result};
-use trader_client::utils::{get_wallet, load_config};
+use trader_client::utils::{
+    get_wallet,
+    load_config,
+    create_mint_ix,
+    create_account_ix,
+};
 use solana_client::rpc_client::RpcClient;
 use spl_associated_token_account;
 use solana_sdk::{
@@ -19,6 +25,10 @@ use solana_sdk::{
     },
     transaction::Transaction,
 };
+use spl_token::state::{
+    Account,
+    Mint,
+};
 use std::borrow::Borrow;
 //use std::fmt::Result;
 use std::str::FromStr;
@@ -26,6 +36,147 @@ use trader::{
     state,
     instructions::Action,
 };
+
+/*
+ * Sets up all base accounts to test all oprations. Expects two wallets, wallet1 will hold Mint account A, wallet2
+ * will hold Mintaccount B.
+ * 
+ * - Mint A
+ * - Mint B
+ * 
+ * A (offer)
+ *   - Token Account 1: Mint A. Offer src account which will be transfered to the trader program, with balance of 1000
+ *   - Token account 2: Mint B. Trade dst account
+ * 
+ * B (trade)
+ *   - Token Account 3: Mint A. Offer dst account
+ *   - Token account 4: Mint B. Trade src account with a balance of 1000
+ * 
+*/
+pub fn setup_accounts(
+    offer: u64,
+    trade: u64,
+    wallet1: Keypair,
+    wallet2: Keypair,
+    conn: &RpcClient,
+) -> Result<()> {
+    let mut ixs = Vec::<Instruction>::new();
+    // Mint A
+    let mint_a_keypair = &Keypair::new();
+    let create_mint_a_ix = create_mint_ix(
+        &wallet1.pubkey(),
+        &mint_a_keypair.pubkey(),
+        conn,
+    );
+    ixs.extend(create_mint_a_ix);
+    
+    // Mint B
+    let mint_b_keypair = &Keypair::new();
+    let create_mint_b_ix = create_mint_ix(
+        &wallet1.pubkey(),
+        &mint_b_keypair.pubkey(),
+        conn,
+    );
+    ixs.extend(create_mint_b_ix);
+
+    let message = Message::new(ixs.as_ref(), Some(&wallet1.pubkey()));
+    let transaction = Transaction::new(&[&wallet1, mint_a_keypair, mint_b_keypair], message, conn.get_latest_blockhash().unwrap());
+    conn.send_and_confirm_transaction(&transaction).unwrap();
+
+    println!("Mint accounts setup finished!");
+
+    // Setup offer
+    let mut ixs = Vec::<Instruction>::new();
+    // Account 1
+    let account_1_keypair = &Keypair::new();
+    let create_account_1_ix = create_account_ix(
+        &wallet1.pubkey(),
+        &mint_a_keypair.pubkey(),
+        &account_1_keypair.pubkey(),
+        conn,
+    );
+    ixs.extend(create_account_1_ix);
+
+    let offer_balance = offer*10u64.pow(9); //9 decimals
+    let mint_offer_ix = spl_token::instruction::mint_to(
+        &spl_token::id(), 
+        &mint_a_keypair.pubkey(),
+        &account_1_keypair.pubkey(),
+        &wallet1.pubkey(),
+        &[&wallet1.pubkey()],
+        offer_balance, 
+    ).unwrap();
+    ixs.push(mint_offer_ix);
+
+    // Account 2
+    let account_2_keypair = &Keypair::new();
+    let create_account_2_ix = create_account_ix(
+        &wallet1.pubkey(),
+        &mint_b_keypair.pubkey(),
+        &account_2_keypair.pubkey(),
+        conn,
+    );
+    ixs.extend(create_account_2_ix);
+
+    let message = Message::new(ixs.as_ref(), Some(&wallet1.pubkey()));
+    let transaction = Transaction::new(&[&wallet1, account_1_keypair, account_2_keypair], message, conn.get_latest_blockhash().unwrap());
+    conn.send_and_confirm_transaction(&transaction).unwrap();
+
+    println!("Offer accounts setup finished!");
+
+    // Setup trade
+    // Account 3
+    let mut ixs = Vec::<Instruction>::new();
+    let account_3_keypair = &Keypair::new();
+    let create_account_3_ix = create_account_ix(
+        &wallet2.pubkey(),
+        &mint_a_keypair.pubkey(),
+        &account_3_keypair.pubkey(),
+        conn,
+    );
+    ixs.extend(create_account_3_ix);
+
+    // Account 4
+    let account_4_keypair = &Keypair::new();
+    let create_account_4_ix = create_account_ix(
+        &wallet2.pubkey(),
+        &mint_b_keypair.pubkey(),
+        &account_4_keypair.pubkey(),
+        conn,
+    );
+    ixs.extend(create_account_4_ix);
+
+    let trade_balance = trade*10u64.pow(9); //9 decimals
+    let mint_trade_ix = spl_token::instruction::mint_to(
+        &spl_token::id(), 
+        &mint_b_keypair.pubkey(),
+        &account_4_keypair.pubkey(),
+        &wallet1.pubkey(),
+        &[&wallet1.pubkey()],
+        trade_balance, 
+    ).unwrap();
+    ixs.push(mint_trade_ix);
+
+    let message = Message::new(ixs.as_ref(), Some(&wallet2.pubkey()));
+    let transaction = Transaction::new(&[&wallet1, &wallet2, account_3_keypair, account_4_keypair], message, conn.get_latest_blockhash().unwrap());
+    conn.send_and_confirm_transaction(&transaction).unwrap();
+
+    println!("Trade accounts setup finished!");
+
+    // report
+    println!("");
+    println!("Mint account A: {}", mint_a_keypair.pubkey().to_string());
+    println!("Mint account B: {}", mint_b_keypair.pubkey().to_string());
+    println!("Offer");
+    println!("\t- src: {} with balance {}", account_1_keypair.pubkey().to_string(), offer);
+    println!("\t- dst: {}", account_3_keypair.pubkey().to_string());
+    println!("Trade");
+    println!("\t- src: {} with balance {}", account_4_keypair.pubkey().to_string(), trade);
+    println!("\t- dst: {}", account_2_keypair.pubkey().to_string());
+    println!(""); 
+
+    Ok(())
+}
 
 pub fn create_trade(
     trade: u64, 
@@ -99,7 +250,7 @@ pub fn make_trade(
     trade_dst: Pubkey,
     trade_src: Pubkey, 
     offer_dst: Pubkey,
-    original_pda: Pubkey,
+    original_pda_addr: Pubkey,
     conn: &RpcClient,
 ) -> Result<()> {
     println!("Making trade...");
@@ -122,12 +273,12 @@ pub fn make_trade(
             AccountMeta::new_readonly(owner.pubkey(), true),
             AccountMeta::new(trade_id, false),
             AccountMeta::new(pda_pubkey, false),
+            AccountMeta::new(original_pda_addr, false),
             AccountMeta::new(trade_dst, false),
             AccountMeta::new(trade_src, false),
             AccountMeta::new(offer_dst, false),
             AccountMeta::new_readonly(spl_token::id(), false),
             AccountMeta::new_readonly(trader_program_id, false),
-            AccountMeta::new(original_pda, false),
         ],
     );
     let message = Message::new(&[make_trade_ix], Some(&owner.pubkey()));
@@ -138,11 +289,19 @@ pub fn make_trade(
     Ok(())
 }
 
+/*
+solana-keygen new --outfile wallet1.json
+solana config set --keypair $(pwd)/wallet1.json
+solana airdrop 50000
+solana-keygen new --outfile wallet2.json
+solana config set --keypair $(pwd)/wallet2.json
+solana airdrop 50000
+ */
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
-    if args.len() != 3 {
+    if args.len() < 3 {
         eprintln!(
-            "usage: {} <path to solana hello world example program keypair>",
+            "usage: {} <trader prog addr> <action>",
             args[0]
         );
         std::process::exit(-1);
@@ -171,26 +330,26 @@ fn main() {
         return;
     }
 
-    let wallet = get_wallet().unwrap();
+    let wallet = get_wallet(None).unwrap();
     //let balance = conn.get_account(&wallet.pubkey()).unwrap().lamports;
     //println!("Account balance: {}", balance); // TODO: this prints a different value from 'solana balance' ?
 
     // TODO
-    let token_pubkey = Pubkey::from_str("J9w7iwsKUdhnPDutBjdYEphbKRpaLUmtpfAGggadupcE").unwrap();
+    let token_pubkey = Pubkey::from_str("6c12WF8zFc6My1oPz7EzNYj5CSzaWP29wES66fhi7Hgr").unwrap();
 
-    let trade_id = Pubkey::from_str("H96F9EAuds2Wab2qpzZXrUG16qy1rwQr9ASncoQgoQ1u").unwrap();
-    let offer_dst = Pubkey::from_str("5AfQJS5FJkk1SQf5s7c5XFBDeHNbQAzjEXMWwDTbixwa").unwrap();
-    let trade_dst = Pubkey::from_str("DscwD8rrC3Ztr4hQqSdtbAhCAubrjADVE4sRYg3U31hi").unwrap();
-    let trade_src = Pubkey::from_str("BsP4W1iW1JHA3nAYMTNAWC26knq7ipWgAxTmQSnqZpaf").unwrap();
-
+    let trade_id = Pubkey::from_str("7eWD3WfMjrYNxnJAH4WBZrPcZcMsf5Cd619nbAVscyLT").unwrap();
+    let offer_dst = Pubkey::from_str("3t4zR1TVwvAh4wBCnmkoHokbqmLLxMrGv7mo95DYhwPq").unwrap();
+    let trade_src = Pubkey::from_str("3zGFKpHaZztezZh2Bs9tpVB4thgu6czoRWmT4mffmo2q").unwrap();
+    let trade_dst = Pubkey::from_str("EsUyS8fpQZAnwcqRzptGhSSoSMpxymKVBrYoJ42txuwg").unwrap();
+    
     //println!("fees = {:?}", rpc.get_fees()?);
     //println!("signature fee = {}", rpc.get_fees()?.fee_calculator.lamports_per_signature);
 
     match args[2].as_str() {
         "1" => create_trade(2, wallet, token_pubkey, program_pubkey, &conn).unwrap(),
         "2" => make_trade(
-            10000000000, // TODO: stop using the account.balance or handle decimals...
-            2,
+            10000000000, // TODO: pass 10 and on-chain adjust with the account decimals
+            20000000000, // TODO: pass 10 and on-chain adjust with the account decimals
             wallet,
             trade_id,
             program_pubkey,
@@ -200,6 +359,18 @@ fn main() {
             token_pubkey,
             &conn,
         ).unwrap(),
+        "3" => {
+            if args.len() != 5 {
+                eprintln!(
+                    "usage: {} <trader prog addr> 3 <path to wallet 1> <path to wallet 2>",
+                    args[0]
+                );
+                std::process::exit(-1);
+            }
+            let wallet1 = get_wallet(Some(&args[3])).unwrap();
+            let wallet2 = get_wallet(Some(&args[4])).unwrap();
+            setup_accounts(10, 12, wallet1, wallet2, &conn).unwrap();
+        }
         op => {
             eprintln!("Unknown operation '{}'", op);
             std::process::exit(-1);
--- client/src/utils.rs
+++ client/src/utils.rs
@@ -1,5 +1,16 @@
 use crate::{Error, Result};
+use solana_client::rpc_client::RpcClient;
+use solana_program::program_pack::Pack;
 use solana_sdk::signer::keypair::{read_keypair_file, Keypair};
+use solana_sdk::{
+    instruction::Instruction,
+    pubkey::Pubkey,
+    system_instruction,
+};
+use spl_token::state::{
+    Account,
+    Mint,
+};
 use yaml_rust::YamlLoader;
 
 pub fn load_config() -> Result<yaml_rust::Yaml> {
@@ -26,17 +37,69 @@ pub fn load_config() -> Result<yaml_rust::Yaml> {
     }
 }
 
-pub fn get_wallet() -> Result<Keypair> {
+pub fn get_wallet(maybe_path: Option<&String>) -> Result<Keypair> {
     let config = load_config()?;
-    let path = match config["keypair_path"].as_str() {
+
+    let path = match maybe_path {
         Some(s) => s,
         None => {
-            return Err(Error::InvalidConfig(
-                "missing `keypair_path` field".to_string(),
-            ))
+            let p = match config["keypair_path"].as_str() {
+                Some(s1) => s1,
+                None => {
+                    return Err(Error::InvalidConfig(
+                        "missing `keypair_path` field".to_string(),
+                    ))
+                }
+            };
+            p
         }
     };
+    
     read_keypair_file(path).map_err(|e| {
         Error::InvalidConfig(format!("failed to read keypair file ({}): ({})", path, e))
     })
+}
+
+pub fn create_mint_ix(authority: &Pubkey, mint_pubkey: &Pubkey, conn: &RpcClient) -> Vec<Instruction> {
+    let create_ix = system_instruction::create_account(
+        authority,
+        mint_pubkey,
+        conn.get_minimum_balance_for_rent_exemption(Mint::LEN).unwrap(),
+        Mint::LEN as u64,
+        &spl_token::id(),
+    );
+
+    let init_ix = spl_token::instruction::initialize_mint(
+        &spl_token::id(), 
+        mint_pubkey,
+        authority, 
+        None, 
+        9,
+    ).unwrap();
+
+    Vec::from([create_ix, init_ix])
+}
+
+pub fn create_account_ix(
+    authority: &Pubkey, 
+    mint_pubkey: &Pubkey,
+    account_pubkey: &Pubkey, 
+    conn: &RpcClient,
+) -> Vec<Instruction> {
+    let create_ix = system_instruction::create_account(
+        authority,
+        account_pubkey,
+        conn.get_minimum_balance_for_rent_exemption(Account::LEN).unwrap(),
+        Account::LEN as u64,
+        &spl_token::id(),
+    );
+
+    let init_ix = spl_token::instruction::initialize_account(
+        &spl_token::id(), 
+        account_pubkey,
+        mint_pubkey,
+        authority, 
+    ).unwrap();
+
+    Vec::from([create_ix, init_ix])
 }
